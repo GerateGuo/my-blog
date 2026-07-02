943d8dc9336dd465096cb757c2e0d97d5aafa857
GerateGuo <vitacookie@qq.com>
2026-07-02 16:21:10 +0800

    修复路径，移除旧修改

--- scripts/inject-custom.js
+++ scripts/inject-custom.js
@@ -1,16 +1,14 @@
 /* global hexo */
 
 hexo.extend.filter.register('after_render:html', function(data) {
-  const root = hexo.config.root || '/';
-
   // 注入自定义 CSS
-  const cssTag = `<link rel="stylesheet" href="${root}css/liquid-glass.css">`;
+  const cssTag = '<link rel="stylesheet" href="/my-blog/css/liquid-glass.css">';
   if (!data.includes('liquid-glass.css')) {
     data = data.replace('</head>', cssTag + '\n</head>');
   }
 
   // 注入自定义 JS
-  const jsTag = `<script src="${root}js/cursor-trail.js"></script>`;
+  const jsTag = '<script src="/my-blog/js/cursor-trail.js"></script>';
   if (!data.includes('cursor-trail.js')) {
     data = data.replace('</body>', jsTag + '\n</body>');
   }
